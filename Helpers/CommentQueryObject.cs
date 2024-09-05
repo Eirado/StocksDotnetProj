81f20d34902fe890b953a801a010f7f2bb524d65
Eirado <GABEIRADO2@HOTMAIL.COm>
2024-09-05 10:39:10 -0300

    Final

--- Helpers/CommentQueryObject.cs
+++
@@ -1,9 +0,0 @@
-
-namespace StocksDotnetProj.Helpers
-{
-    public class CommentQueryObject
-    {
-        public string Symbol { get; set; }
-        public bool IsDecsending { get; set; } = true;
-    }
-}
